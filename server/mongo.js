cd62db1c2a0093824e235febf97673dbe89b1c3b
DESKTOP-MFBDCAC\羊咩咩 <kei91065@gmail.com>
2022-04-11 00:24:28 +0800

    Revert "update environment"
    This reverts commit a2527b47faaffe776a4c296cf173ea3397e23dec.

--- server/mongo.js
+++ server/mongo.js
@@ -3,12 +3,10 @@ const env = require('./env/environment');
 
 mongoose.Promise = global.Promise;
 
-//const mongoUri = `mongodb://${env.dbName}:${env.key}@${env.dbName}.mongo.cosmos.azure.com:${env.cosmosPort}/?ssl/=true`;
+const mongoUri = `mongodb://${env.dbName}.documents.azure.com:${env.cosmosPort}/?ssl/=true`;
 
-const mongoUri = 'mongodb://react-cosmo-db:7J4Qw4HZEfGyjPb6WezwA5f6nhUcLTsgH2XvspvqC1pAjHE5xARSQNhKnYYCEUs6kTWinp46S6xj4eRm5kyT3A==@react-cosmo-db.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&appName=@react-cosmo-db@';
 function connect() {
-  //return mongoose.connect(mongoUri, { auth: { user: env.dbName, password: env.key }});
-  return mongoose.connect(mongoUri, { useMongoClient: true});
+  return mongoose.connect(mongoUri, { auth: { user: env.dbName, password: env.key }});
 }
 
 module.exports = {
