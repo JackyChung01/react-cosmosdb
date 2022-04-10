143d17869341f81018833d825d8abe0ce6d4154a
DESKTOP-MFBDCAC\羊咩咩 <kei91065@gmail.com>
2022-04-11 00:24:43 +0800

    Revert "Revert "update environment""
    This reverts commit cd62db1c2a0093824e235febf97673dbe89b1c3b.

---
+++ server/env/environment.js
@@ -0,0 +1,9 @@
+const cosmosPort = 10255; // replace with your port
+const dbName = 'react-cosmo-db';
+const key = '7J4Qw4HZEfGyjPb6WezwA5f6nhUcLTsgH2XvspvqC1pAjHE5xARSQNhKnYYCEUs6kTWinp46S6xj4eRm5kyT3A==';
+
+module.exports = {
+  cosmosPort,
+  dbName,
+  key
+};
